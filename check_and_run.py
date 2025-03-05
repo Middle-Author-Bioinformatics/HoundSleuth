3772d5d6d63f7260819fb7df9ee396302eef0595
Middle Author Bioinformatics <dna@midauthorbio.com>
2025-03-05 16:31:03 -0500

    Update check_and_run.py

--- check_and_run.py
+++ check_and_run.py
@@ -7,8 +7,8 @@ import subprocess
 
 # S3 bucket name and folder prefix
 BUCKET_NAME = 'breseqbucket'
-FOLDER_PREFIX = 'midis-'
-LOG_FILE = '/home/ark/MAB/midis/processed_folders.log'
+FOLDER_PREFIX = 'houndsleuth-'
+LOG_FILE = '/home/ark/MAB/houndsleuth/processed_folders.log'
 
 # Initialize S3 client (using default credentials)
 s3 = boto3.client('s3')
@@ -77,17 +77,17 @@ def download_folder(bucket_name, folder_name, local_dir):
 
 def process_folder(folder_name):
     """Process the folder by downloading its contents and performing analysis."""
-    local_dir = f"/home/ark/MAB/midis/{folder_name}"
+    local_dir = f"/home/ark/MAB/houndsleuth/{folder_name}"
     print(f"Processing folder: {folder_name}")
     download_folder(BUCKET_NAME, folder_name, local_dir)
 
-    # Run midis.sh script with folder_name as argument
-    midis_script_path = '/home/ark/MAB/bin/midis-local/midis.sh'
+    # Run houndsleuth.sh script with folder_name as argument
+    houndsleuth_script_path = '/home/ark/MAB/bin/HoundSleuth/houndsleuth.sh'
     try:
-        subprocess.run([midis_script_path, folder_name], check=True)
-        print(f"Successfully processed folder {folder_name} using midis.sh.")
+        subprocess.run([houndsleuth_script_path, folder_name], check=True)
+        print(f"Successfully processed folder {folder_name} using houndsleuth.sh.")
     except subprocess.CalledProcessError as e:
-        print(f"Error running midis.sh on folder {folder_name}: {e}")
+        print(f"Error running houndsleuth.sh on folder {folder_name}: {e}")
 
 def main():
     """Main function to check for new folders and trigger processing."""
