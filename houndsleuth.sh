c540ee3b17ae3bb08b53a304ed19bd2e0f424947
Middle Author Bioinformatics <dna@midauthorbio.com>
2025-03-04 21:09:35 -0500

    init

--- houndsleuth.sh
+++ houndsleuth.sh
@@ -2,7 +2,7 @@
 eval "$(/home/ark/miniconda3/bin/conda shell.bash hook)"
 conda activate base  # Activate the base environment where `boto3` is installed
 
-exec > >(tee -i /home/ark/MAB/midis/midis_looper.log)
+exec > >(tee -i /home/ark/MAB/houndsleuth/houndsleuth_looper.log)
 exec 2>&1
 
 eval "$(/home/ark/miniconda3/bin/conda shell.bash hook)"
@@ -10,23 +10,23 @@ conda activate base  # Activate the base environment where `boto3` is installed
 
 KEY=$1
 ID=$KEY
-DIR=/home/ark/MAB/midis/${ID}
-OUT=/home/ark/MAB/midis/completed/${ID}-results
+DIR=/home/ark/MAB/houndsleuth/${ID}
+OUT=/home/ark/MAB/houndsleuth/completed/${ID}-results
 
 
 name=$(grep 'Name' ${DIR}/form-data.txt | cut -d ' ' -f2)
 email=$(grep 'Email' ${DIR}/form-data.txt | cut -d ' ' -f2)
 input=$(grep 'Input' ${DIR}/form-data.txt | cut -d ' ' -f2)
-echo input
+echo $input
 
 # Verify email
-result=$(python3 /home/ark/MAB/bin/midis-local/check_email.py --email ${email})
+result=$(python3 /home/ark/MAB/bin/HoundSleuth/check_email.py --email ${email})
 echo $result
 
 # Set PATH to include Conda and script locations
-export PATH="/home/ark/miniconda3/bin:/usr/local/bin:/usr/bin:/bin:/home/ark/MAB/bin/midis-local:$PATH"
+export PATH="/home/ark/miniconda3/bin:/usr/local/bin:/usr/bin:/bin:/home/ark/MAB/bin/HoundSleuth:$PATH"
 eval "$(/home/ark/miniconda3/bin/conda shell.bash hook)"
-conda activate midis
+conda activate houndsleuth
 
 if [ $? -ne 0 ]; then
     echo "Error: Failed to activate Conda environment."
@@ -38,14 +38,14 @@ sleep 5
 # **************************************************************************************************
 # **************************************************************************************************
 # **************************************************************************************************
-# Run MagicLamp
-/home/ark/MAB/bin/MagicLamp/MagicLamp.py --key ${KEY} --input ${input} --output ${OUT}
+# Run HoundsSleuth
+/home/ark/MAB/bin/SprayNPray/spray-and-pray.py -g ${DIR}/${input} -out ${OUT} -ref /home/ark/databases/nr.dmnd --hits 1 -t 20
 
 # **************************************************************************************************
 # **************************************************************************************************
 # **************************************************************************************************
 if [ $? -ne 0 ]; then
-    echo "Error: MagicLamp failed."
+    echo "Error: HoundSleuth failed."
     conda deactivate
     exit 1
 fi
@@ -53,29 +53,27 @@ conda deactivate
 sleep 5
 
 # Archive results
-mv /home/ark/MAB/magiclamp/completed/${ID}-results ./${ID}-results
+mv /home/ark/MAB/houndsleuth/completed/${ID}-results ./${ID}-results
 tar -cf ${ID}-results.tar ${ID}-results && gzip ${ID}-results.tar
 
 # Upload results to S3 and generate presigned URL
 results_tar="${ID}-results.tar.gz"
 s3_key="${ID}-results.tar.gz"
-python3 /home/ark/MAB/bin/magiclamp-local/push.py --bucket binfo-dump --output_key ${s3_key} --source ${results_tar}
-url=$(python3 /home/ark/MAB/bin/magiclamp-local/gen_presign_url.py --bucket binfo-dump --key ${s3_key} --expiration 86400)
+python3 /home/ark/MAB/bin/HoundSleuth/push.py --bucket binfo-dump --output_key ${s3_key} --source ${results_tar}
+url=$(python3 /home/ark/MAB/bin/HoundSleuth/gen_presign_url.py --bucket binfo-dump --key ${s3_key} --expiration 86400)
 
-mv ${ID}-results.tar.gz /home/ark/MAB/magiclamp/completed/${ID}-results.tar.gz
+mv ${ID}-results.tar.gz /home/ark/MAB/houndsleuth/completed/${ID}-results.tar.gz
 
 # Send email
-python3 /home/ark/MAB/bin/magiclamp-local/send_email.py \
+python3 /home/ark/MAB/bin/HoundSleuth/send_email.py \
     --sender ark@midauthorbio.com \
     --recipient ${email} \
-    --subject "Your MagicLamp Results!" \
+    --subject "Your HoundSleuth Results!" \
     --body "Hi ${name},
 
-    Your MagicLamp results are available for download using the link below. The link will expire in 24 hours.
+    Your HoundSleuth results are available for download using the link below. The link will expire in 24 hours.
 
     ${url}
-
-    Please visit https://github.com/Arkadiy-Garber/MagicLamp for documentation.
 
     Please reach out to ark@midauthorbio.com, or send us a note on https://midauthorbio.com/#contact if you have any questions.
 
