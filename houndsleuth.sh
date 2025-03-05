a2ba213d561bfc92db9e0d5ed09cf15410018a56
Middle Author Bioinformatics <dna@midauthorbio.com>
2025-03-04 23:19:27 -0500

    init

--- houndsleuth.sh
+++ houndsleuth.sh
@@ -39,7 +39,13 @@ sleep 5
 # **************************************************************************************************
 # **************************************************************************************************
 # Run HoundsSleuth
-/home/ark/MAB/bin/SprayNPray/spray-and-pray.py -g ${DIR}/${input} -out ${OUT} -ref /home/ark/databases/nr.dmnd --hits 1 -t 20
+mkdir -p ${OUT}
+mkdir -p ${OUT}/binarena
+mkdir -p ${OUT}/spraynpray
+
+/home/ark/MAB/bin/SprayNPray/spray-and-pray.py -g ${DIR}/${input} -out ${OUT}/spraynpray -ref /home/ark/databases/nr.dmnd --hits 1 -t 20
+
+/home/ark/MAB/bin/HoundSleuth/binstage.sh -i ${DIR}/${input} -o ${OUT}/binarena/${input%.*} -s ${OUT}/spraynpray/spraynpray.csv -m 300
 
 # **************************************************************************************************
 # **************************************************************************************************
