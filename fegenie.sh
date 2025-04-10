5f74189efb9e87834798f2e8a863828d75bf2a61
Middle Author Bioinformatics <dna@midauthorbio.com>
2025-04-09 22:50:08 -0400

    Update fegenie.sh

--- fegenie.sh
+++ fegenie.sh
@@ -37,7 +37,7 @@ sleep 5
 # **************************************************************************************************
 # Run FeGenie
 mkdir -p ${OUT}
-for file in ${DIR}/*; do [[ ${file} == ${DIR}/form-data.txt ]] || mv -f $i ${file%.*}.fa; done
+for file in ${DIR}/*; do [[ ${file} == ${DIR}/form-data.txt ]] || mv -f ${file} ${file%.*}.fa; done
 /home/ark/bin/FeGenie/FeGenie.py -bin_dir ${DIR}/ -bin_ext fa -out ${OUT} -t 16 --nohup
 
 # **************************************************************************************************
