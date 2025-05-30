3773e8eb75bfd67ce8a4adaf8577bdf372bfde6d
Middle Author Bioinformatics <dna@midauthorbio.com>
2025-05-29 17:40:26 -0700

    Update binarena-combine.py

--- binarena-combine.py
+++ binarena-combine.py
@@ -43,11 +43,15 @@ args = parser.parse_known_args()[0]
 
 
 def combine_tsv_files(folder_path):
-    basic_name = args.b + ".basic.tsv"
-    basic_name = lastItem(basic_name.split("/"))
-    realfolder_path = allButTheLast(args.b, "/")
+    # basic_name = args.o + ".basic.tsv"
+    basic_name = lastItem(args.b) + ".basic.tsv"
+    print(basic_name)
+    # realfolder_path = allButTheLast(args.b, "/")
+    print(folder_path)
     # print(basic_name)
-    basic_file_path = folder_path + ".basic.tsv"
+    basic_file_path = folder_path + "/" + basic_name
+    print(basic_file_path)
+    print("+++++++++")
     # print(basic_file_path)
 
     if not os.path.exists(basic_file_path):
@@ -62,12 +66,12 @@ def combine_tsv_files(folder_path):
     # print("Filtering for files containing:", args.b)
 
     BIN = os.path.basename(args.b)
-    tsv_files = [f for f in os.listdir(realfolder_path) if f.endswith('.tsv') and f != basic_name and BIN in f]
+    tsv_files = [f for f in os.listdir(folder_path) if f.endswith('.tsv') and f != basic_name and BIN in f]
 
     print("Matched TSV files:", tsv_files)
 
     for tsv_file in tsv_files:
-        file_path = os.path.join(realfolder_path, tsv_file)
+        file_path = os.path.join(folder_path, tsv_file)
         print(file_path)
         dissimilarity_df = pd.read_csv(file_path, sep='\t')
         combined_df = pd.merge(combined_df, dissimilarity_df, on=combined_df.columns[0], how='left')
