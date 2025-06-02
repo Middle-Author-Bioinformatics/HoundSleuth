ba073c24849d21286776e62b446a34302d1d12bb
Middle Author Bioinformatics <dna@midauthorbio.com>
2025-06-02 01:59:20 -0400

    Update binstager.py

--- binstager.py
+++ binstager.py
@@ -133,6 +133,12 @@ for i in summary:
                 for j in taxa:
                     tax = (j.split(" ")[0])
                     tax = tax.split(";")[0]
+                    if tax == "unclassified":
+                        try:
+                            tax = (j.split(" ")[1])
+                            tax = tax.split(";")[0]
+                        except IndexError:
+                            tax = "unclassified"
                     taxaDict[tax].append(tax)
 
                 taxaDict2 = defaultdict(lambda: '-')
