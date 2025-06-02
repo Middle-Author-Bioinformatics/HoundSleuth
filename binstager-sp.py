442740fd04afd86a6bca1c23e97ca9ccb2f5e6ce
Middle Author Bioinformatics <dna@midauthorbio.com>
2025-06-01 20:20:35 -0400

    Update binstager-sp.py

--- binstager-sp.py
+++ binstager-sp.py
@@ -138,7 +138,7 @@ for i in summary:
                     try:
                         tax = (j.split(" ")[1])
                         tax = tax.split(";")[0]
-                        if tax not in ["sp.", "bacterium", "synthetase", "taxa", "group", "", "L-lysine-forming", "prokaryote", "restricta"] and tax[0].isupper():
+                        if tax not in ["sp.", "bacterium", "synthetase", "taxa", "group", "", "L-lysine-forming", "prokaryote", "restricta"] and not tax[0].isupper():
                             taxaDict[tax].append(tax)
                     except IndexError:
                         tax = j.split(";")[0]
