036643e1d82a5ce42ea41a892f06758677f53711
Middle Author Bioinformatics <dna@midauthorbio.com>
2025-08-21 22:56:12 -0700

    Update binstager.py

--- binstager.py
+++ binstager.py
@@ -159,8 +159,8 @@ for i in summary:
                     else:
                         winningTaxa = 'unclassified'
 
-                summaryDict[ls[0]] = winningTaxa
-                splitDict[winningTaxa].append(ls[0])
+                summaryDict[ls[0].split(" ")[0]] = winningTaxa
+                splitDict[winningTaxa].append(ls[0].split(" ")[0])
                 print(ls[0], winningTaxa)
 
 
