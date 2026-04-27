a20fd779607d8ef4fbb1642c9b9bdb5cb73348bf
Middle Author Bioinformatics <dna@midauthorbio.com>
2026-04-27 17:15:05 -0400

    html report

--- fegenie.sh
+++ fegenie.sh
@@ -38,7 +38,9 @@ sleep 5
 # Run FeGenie
 mkdir -p ${OUT}
 for file in ${DIR}/*; do [[ ${file} == ${DIR}/form-data.txt ]] || mv -f ${file} ${file%.*}.fa; done
-/home/ark/bin/FeGenie/FeGenie.py -bin_dir ${DIR}/ -bin_ext fa -out ${OUT} -t 16 --nohup --makeplots
+/home/ark/bin/FeGenie/FeGenie.py -bin_dir ${DIR}/ -bin_ext fa -out ${OUT} -t 16 --nohup
+
+python /home/ark/MAB/bin/HoundSleuth/fegenie_report.py ${OUT}/FeGenie-heatmap-data.csv ${OUT}/FeGenie-report.html
 
 # **************************************************************************************************
 # **************************************************************************************************
